4da093ac9bfcc5edf4fbb7a19a91dd2194aabf1c
alumno <alumno@utnfra.com.ar>
2020-02-07 22:13:44 -0300

    tps, parciales y recuperatorios

--- 8-TPs/jsFerretePinturas.js
+++ 8-TPs/jsFerretePinturas.js
@@ -4,10 +4,30 @@ B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
 */
 function FahrenheitCentigrados () 
 {
-	
+    var temperatura;
+    var Fahrenheit;
+    var Centígrados;
+
+    Fahrenheit = document.getElementById("Temperatura").value
+    Fahrenheit = parseFloat(Fahrenheit);
+
+    Centígrados = (((Fahrenheit - 32)/1.8))
+
+    alert( Fahrenheit + " Fahrenheit son " + Centígrados + " Centígrados")
+
 }
 
 function CentigradosFahrenheit () 
 {
-	
+	var temperatura;
+    var Fahrenheit;
+    var Centígrados;
+
+    Centígrados = document.getElementById("Temperatura").value
+    Centígrados = parseFloat(Centígrados);
+
+    Fahrenheit = (( (9/5) * Centígrados ) + 32)
+
+    alert( Centígrados + " Centígrados son " + Fahrenheit + " Fahrenheit")
+
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,21 @@
 
 function mostrar()
 {
+    var ancho;
+    var largo;
+    var perimetro;
+
+    var ancho = prompt("Ingrese el ancho del rectangulo");
+    ancho = parseFloat(ancho);
+
+    var largo = prompt("Ingrese el largo del rectangulo");
+    largo = parseFloat(largo);
+
+    perimetro = ((ancho*largo)*2)
+
+    alert ("El perimetro de su rectangulo de " + ancho + " de ancho y " + largo + " de largo, es " + perimetro)
+
+
+    
 
 }
