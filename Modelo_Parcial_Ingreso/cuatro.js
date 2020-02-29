bda3c97101aabb39d96c90daa3fb4fda5be3a103
alumno <alumno@utnfra.com.ar>
2020-02-28 22:04:22 -0300

    tres y cuatro

--- Modelo_Parcial_Ingreso/cuatro.js
+++ Modelo_Parcial_Ingreso/cuatro.js
@@ -1,4 +1,20 @@
 function mostrar()
 {
 
-}
+    var Letra;
+    var Numero;
+
+    Letra = prompt ("Ingrese una letra ").toLowerCase()
+    do {
+
+    while ( Letra < "a" || letra > "z" )
+    {
+        Letra = prompt ("Error, Vuelva a ingresar la letra ").toLowerCase()
+    }
+
+    while ( Numero < -10 || Numero > 10 )
+    {
+        numero = prompt ("Error, vuelva a ingresar el numero")
+        numero = parseInt (numero)
+    }
+    }
--- Modelo_Parcial_Ingreso/tres.js
+++ Modelo_Parcial_Ingreso/tres.js
@@ -1,37 +1,59 @@
 function mostrar()
 {
 
-    // Ingreso las variables
-
     var NombrePais;
+    var ContadorPaises = 0;
+    var PaisConMasHabitantes;
     var CantidadHabitantes;
+    var MaximaCantidadHabitantes;
     var TemperaturaMinima;
-    var IngresoPaises;
+    var TemperaturaMinimaContador = 0;
+    var CantidadTemperaturaMinima =0;
+    var SeguirIngresoPaises;
+  
+        
+    for ( var i = 0 ; i != "si" ; i++ )
+    {
 
     NombrePais = prompt ("Ingrese el nombre del pais: ");
+
     CantidadHabitantes = prompt ("Ingrese la cantidad de habitantes en millones: ");
     CantidadHabitantes = parseInt (CantidadHabitantes);
 
-    while ( CantidadHabitantes < 1  ||  CantidadHabitantes > 270)
-    {
-        alert ("Error, el rango de habitantes tiene que ir de 1 a 270 millones");
-    }
+        while (isNaN (CantidadHabitantes) || CantidadHabitantes < 1  ||  CantidadHabitantes > 270)
+        {
+            CantidadHabitantes = prompt ("Error, el rango de habitantes tiene que ir de 1 a 270 millones, vuelva a ingresar");
+            CantidadHabitantes = parseInt (CantidadHabitantes);
+        }
 
     TemperaturaMinima = prompt ("Ingrese el valor de la temperatura minima: ");
     TemperaturaMinima = parseFloat (TemperaturaMinima);
 
-    while ( isNaN (TemperaturaMinima) ||  TemperaturaMinima < -50 || TemperaturaMinima > 50 )
+        while ( isNaN (TemperaturaMinima) ||  TemperaturaMinima < -50 || TemperaturaMinima > 50 )
+        {
+            TemperaturaMinima = prompt ("Error, el rango de temperatura tiene que ir de -50 a 50");
+            TemperaturaMinima = parseFloat (TemperaturaMinima);
+        }
+
+    SeguirIngresoPaises = prompt ("Quiere ingresar otro pais? ingrese si/no como respuesta: ")
+
+
+
+        
+
+    if ( TemperaturaMinima != 1)
     {
-        alert ("Error, el rango de temperatura tiene que ir de -50 a 50")
+        TemperaturaMinimaContador += TemperaturaMinima
+        CantidadTemperaturaMinima ++
+
     }
 
-    IngresoPaises = prompt ("Quiere ingresar otro pais? ingrese si/no como respuesta: ")
-
-    while (IngresoPaises == "si")
+    if ( MaximaCantidadHabitantes =6 )
     {
-
+        MaximaCantidadHabitantes = CantidadHabitantes
+        PaisConMasHabitantes = NombrePais
     }
 
-
+    }
 
 }
